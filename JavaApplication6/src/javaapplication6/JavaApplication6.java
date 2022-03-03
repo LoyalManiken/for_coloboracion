97a6dcd051667623d43656fdd9d0d709cd8fd3f8
21598 <21598@1k129s3.dune.edu>
2022-03-03 14:52:14 +0200

    cc

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -1,12 +1,12 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template inghghghe editor.
  */
 package javaapplication6;
 
 /**
- *ghghghg98
+ *ghgbnbnbnb
  */
 public class JavaApplication6 {
 
